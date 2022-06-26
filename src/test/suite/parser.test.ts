0073d3e1ec43fc5755f87d001c84ac12e18a555c
Haryo Akbarianto <haryo97@gmail.com>
2022-06-26 12:14:18 +0700

    fix: add __dirname to see error

--- src/test/suite/parser.test.ts
+++ src/test/suite/parser.test.ts
@@ -2,6 +2,8 @@
  * All parser test goes here
  */
 
+import * as path from 'path';
+
 import { CairoParser } from "./../../lib/parser";
 import * as fs from 'fs';
 suite("Parser Test Suite", () => {
@@ -11,7 +13,8 @@ suite("Parser Test Suite", () => {
     let parser = new CairoParser();
 
     // first, we read the contract from a file on test_utils/ERC20.cairo.
-    const text = fs.readFileSync('./../../test_utils/ERC20.cairo', 'utf8');
+    const pathFile = path.resolve(__dirname, 'test_utils/ERC20.cairo');
+    const text = fs.readFileSync(pathFile, 'utf8');
 
     // It will throw error if it doesn't parse correctly!
     parser.parseAFile(text, "ERC20.cairo");
