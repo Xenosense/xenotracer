d5a4c5c6fbcdfa4951ab34d15d6ecc3113488798
Haryo Akbarianto <haryo97@gmail.com>
2022-06-26 12:17:17 +0700

    fix: fix test_utils path on `parser.test.ts`

--- src/test/suite/parser.test.ts
+++ src/test/suite/parser.test.ts
@@ -13,7 +13,7 @@ suite("Parser Test Suite", () => {
     let parser = new CairoParser();
 
     // first, we read the contract from a file on test_utils/ERC20.cairo.
-    const pathFile = path.resolve(__dirname, 'test_utils/ERC20.cairo');
+    const pathFile = path.resolve(__dirname, '../../../test_utils/ERC20.cairo');
     const text = fs.readFileSync(pathFile, 'utf8');
 
     // It will throw error if it doesn't parse correctly!
