10edcf9d5f1ad19d7c679388e78fffb57f2d50c5
jedi <jedi@pc-2-239.customer.ask4.lan>
2022-06-27 23:08:40 +0100

    docs(node.conditional): fix comments

--- src/lib/nodes/conditionalNode.ts
+++ src/lib/nodes/conditionalNode.ts
@@ -6,7 +6,7 @@ import { EntitiesType } from "../enumCollectionsAndUtils";
 
 export default class CairoConditionalNode extends BaseNode {
   /**
-   * Creates an instance of with_attr node.
+   * Creates an instance of if node.
    * @param name name of the node
    * @param startLine start line of the node
    * @param parents parents node. If you need something to do with parents, you can use this.
@@ -16,8 +16,8 @@ export default class CairoConditionalNode extends BaseNode {
   }
 
   /**
-   * This function will check if the line is a with_attr.
-   * Just check the `with_attr` keyword in it
+   * This function will check if the line is a if.
+   * Just check the `if` keyword in it
    * @param textLine text line !
    * @param currentRunningNodeStack current running node stack (parents)
    *
@@ -51,8 +51,8 @@ export default class CairoConditionalNode extends BaseNode {
 
   /**
    *
-   * Parse with_attr name from the text line
-   * for example, with_attr xxx, parse the `with_attr`.
+   * Parse if name from the text line
+   * for example, if xxx, parse the `if`.
    *
    * @param textLine should be empty string.
    * @param lineNumber should be 0
