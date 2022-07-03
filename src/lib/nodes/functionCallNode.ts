8563bc4997ed65c641006fe75a478678b7e9357b
jedi <amajidsinar@gmail.com>
2022-07-03 19:02:19 +0100

    fix(functioncallnode): simplify condition without namespace

--- src/lib/nodes/functionCallNode.ts
+++ src/lib/nodes/functionCallNode.ts
@@ -1,19 +1,26 @@
 /**
  * A collections of Node
  */
+import { match } from "assert";
 import { BaseNode } from "../base";
 import { EntitiesType } from "../enumCollectionsAndUtils";
 
 
 export default class CairoFunctionCallNode extends BaseNode {
+  namespaceName: string;
+  functionCallName: string;
+
   /**
    * Creates an instance of namespace node.
    * @param name name of the node
    * @param startLine start line of the node
    * @param parents parents node. If you need something to do with parents, you can use this.
    */
-  constructor(name: string, startLine: number, parents: BaseNode[]) {
+  constructor(name: string, startLine: number, parents: BaseNode[], namespaceName: string, functionCallName: string) {
     super(name, startLine, parents, EntitiesType.functionCall);
+    this.namespaceName = namespaceName;
+    this.functionCallName = functionCallName;
+
   }
 
   /**
@@ -77,24 +84,20 @@ export default class CairoFunctionCallNode extends BaseNode {
     const regexWithNamespace = /(\w+)\.(\w+)\([\w\s\,]*\)/;
     const matchWithNamespace = regexWithNamespace.exec(textLine);
     if (matchWithNamespace) {
-      const name: string = matchWithNamespace[1].concat("-",matchWithNamespace[2], "-", lineNumber.toString());
-      return new CairoFunctionCallNode(name, lineNumber, parents);
+      const namespaceName = matchWithNamespace[1];
+      const functionCallName = matchWithNamespace[2];
+      const name: string = namespaceName.concat("-",functionCallName, "-", lineNumber.toString());
+      return new CairoFunctionCallNode(name, lineNumber, parents, namespaceName, functionCallName);
     }
     // if not using namespace
     const regexWithoutNamespace = /(\w+)\([\w\s]*,/
     const matchWithoutNamespace = regexWithoutNamespace.exec(textLine);
     
     if (matchWithoutNamespace) {
-      const namespace = regexWithNamespace.exec(textLine);
-
-      if (namespace){
-        const name: string = matchWithoutNamespace[1].concat(namespace[1].toString(), "-", lineNumber.toString());
-        return new CairoFunctionCallNode(name, lineNumber, parents);
-      }
-      else{
-        const name: string = "null".concat("-", matchWithoutNamespace[1], "-", lineNumber.toString());
-        return new CairoFunctionCallNode(name, lineNumber, parents);
-      }
+        const namespaceName = "null"
+        const functionCallName = matchWithoutNamespace[1]
+        const name: string = namespaceName.concat("-", functionCallName, "-", lineNumber.toString());
+        return new CairoFunctionCallNode(name, lineNumber, parents, namespaceName, functionCallName);
     }
     throw new Error(
       "Cannot create namespace node, invalid text line on line " + lineNumber
